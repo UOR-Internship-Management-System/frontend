1e8bd16e8347def526e6be62696ec4548ddf58f6
Kavindu <kavinduuni@gmail.com>
2026-07-08 02:59:02 +0530

    feat: add role constants

--- src/shared/security/permissions.ts
+++ src/shared/security/permissions.ts
@@ -1,1 +1,6 @@
-export const permissions = {};\n
+export const userRoles = {
+  student: 'STUDENT',
+  admin: 'ADMIN',
+} as const
+
+export type UserRole = (typeof userRoles)[keyof typeof userRoles]
